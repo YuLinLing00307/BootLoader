ca2446882f484951346c3b1bb16b78750a054a5f
zgb <1647429949@qq.com>
2026-05-13 17:24:42 +0800

    完善W25Q32驱动

--- Drivers/INTERFACE/Int_W25Q32.c
+++ Drivers/INTERFACE/Int_W25Q32.c
@@ -40,3 +40,112 @@ void Int_W25Q32_Read_ID(uint8_t* mf_id,uint16_t* device_id)
     Int_W25Q32_Stop();
 }
 
+// 静态方法: 等待芯片忙状态
+static void Int_W25Q32_Wait_Busy(void)
+{
+    // 1.拉低片选
+    Int_W25Q32_Start();
+
+    // 2.读取状态寄存器
+    while(1)
+    {
+        Int_W25Q32_Write_Byte(W25Q32_STATUS_REG);
+        uint8_t status = Int_W25Q32_Read_Byte();
+        if((status & 0x01) == 0) // 判断最后一位即忙位是否为0
+        {
+            break;
+        }
+    }
+
+    // 3.拉高片选
+    Int_W25Q32_Stop();
+}
+
+static void Int_W25Q32_Write_Enable(void)
+{
+    // 1.等待忙状态
+    Int_W25Q32_Wait_Busy();
+
+    // 2.拉低片选
+    Int_W25Q32_Start();
+
+    // 3.发送写使能
+    Int_W25Q32_Write_Byte(W25Q32_WRITE_EN);
+
+    // 4.拉高片选
+    Int_W25Q32_Stop();
+}
+
+void Int_W25Q32_Read_Data(uint8_t Block,uint8_t Sector,uint8_t Page,uint8_t Addr,uint8_t* data,uint16_t len)
+{
+    // 1.等待忙状态结束
+    Int_W25Q32_Wait_Busy();
+
+    // 2.拉低片选
+    Int_W25Q32_Start();
+
+    // 3.发送读取指令,接收返回的数据
+    Int_W25Q32_Write_Byte(W25Q32_READ_COMMAND);
+    uint32_t addr_32 = Block << 16 | Sector << 12 | Page << 8 | Addr;
+    Int_W25Q32_Write_Byte((uint8_t)((addr_32 >> 16) & 0XFF));
+    Int_W25Q32_Write_Byte((uint8_t)((addr_32 >> 8 ) & 0XFF));
+    Int_W25Q32_Write_Byte((uint8_t)((addr_32 >> 0 ) & 0XFF));
+
+    for(uint32_t i;i<len;i++)
+    {
+        data[i] = Int_W25Q32_Read_Byte();
+    }
+    
+    // 4.拉高片选
+    Int_W25Q32_Stop();
+}
+
+void Int_W25Q32_Write_Data(uint8_t Block,uint8_t Sector,uint8_t Page,uint8_t Addr,uint8_t* data,uint16_t len)
+{
+    // 写使能
+    Int_W25Q32_Write_Enable();
+
+    // 1.等待忙状态
+    Int_W25Q32_Wait_Busy();
+
+    // 2.拉低片选
+    Int_W25Q32_Start();
+
+    // 3.写入数据
+    uint32_t addr_32 = Block << 16 | Sector << 12 | Page << 8 | Addr;
+    Int_W25Q32_Write_Byte(W25Q32_WRITE_COMMAND);
+    Int_W25Q32_Write_Byte((uint8_t)((addr_32 >> 16) & 0XFF));
+    Int_W25Q32_Write_Byte((uint8_t)((addr_32 >> 8 ) & 0XFF));
+    Int_W25Q32_Write_Byte((uint8_t)((addr_32 >> 0 ) & 0XFF));
+
+    for (uint16_t i = 0; i < len; i++)
+    {
+        Int_W25Q32_Write_Byte(data[i]);
+    }
+    
+    // 4.拉高片选
+    Int_W25Q32_Stop();
+}
+
+void Int_W25Q32_Erase_Sector(uint8_t Block,uint8_t Sector)
+{
+    // 写使能
+    Int_W25Q32_Write_Enable();
+
+    // 1.等待忙状态
+    Int_W25Q32_Wait_Busy();
+    
+    // 2.拉低片选
+    Int_W25Q32_Start();
+    
+    // 3.发送擦除指令
+    Int_W25Q32_Write_Byte(W25Q32_ERASE_SECTOR_COMMAND);
+    uint32_t addr_32 = Block << 16 | Sector << 12;
+    Int_W25Q32_Write_Byte((uint8_t)((addr_32 >> 16) & 0XFF));
+    Int_W25Q32_Write_Byte((uint8_t)((addr_32 >> 8 ) & 0XFF));
+    Int_W25Q32_Write_Byte((uint8_t)((addr_32 >> 0 ) & 0XFF));
+    
+    // 4.拉高片选
+    Int_W25Q32_Stop();
+}
+
--- Drivers/INTERFACE/Int_W25Q32.h
+++ Drivers/INTERFACE/Int_W25Q32.h
@@ -7,12 +7,26 @@
 // SPI设备在使用的时候片选引脚需要拉低，平时都拉高
 
 // 命令声明
-#define W25Q32_READ_ID 0X9F
+#define W25Q32_READ_ID              0X9F
+#define W25Q32_READ_COMMAND         0X03
+#define W25Q32_WRITE_EN             0X06
+#define W25Q32_WRITE_COMMAND        0X02
+#define W25Q32_ERASE_SECTOR_COMMAND 0X20
+#define W25Q32_STATUS_REG           0X05
 
 void Int_W25Q32_Start(void);
 void Int_W25Q32_Stop(void);
 void Int_W25Q32_Write_Byte(uint8_t data);
 uint8_t Int_W25Q32_Read_Byte(void);
 
+// 实际ADDR共有24位 0x000000 -> 0x3FF FFF 一次擦除4096字节即0xFFF，一次写入256字节
+//void Int_W25Q32_Read_Data(uint32_t addr,uint8_t* data,uint8_t len);
+
+// 采用把地址拆分的方式,更加直观
+void Int_W25Q32_Read_Data(uint8_t Block,uint8_t Sector,uint8_t Page,uint8_t Addr,uint8_t* data,uint16_t len);
+
+void Int_W25Q32_Write_Data(uint8_t Block,uint8_t Sector,uint8_t Page,uint8_t Addr,uint8_t* data,uint16_t len);
+
+void Int_W25Q32_Erase_Sector(uint8_t Block,uint8_t Sector);
 
 #endif
